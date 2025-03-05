c34bcfa00d663132f6a08d69e774f64d19b88ba9
Jargens <angelacosta0@hotmail.com>
2025-03-05 17:48:07 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(function(registration) {
+            console.log('Service Worker registrado con éxito:', registration);
+        })
+        .catch(function(error) {
+            console.error('Error al registrar el Service Worker:', error);
+        });
 }
